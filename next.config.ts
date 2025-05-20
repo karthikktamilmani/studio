e1d1004e719e01081c9f81cc5385217b0e7410a8
Karthikk Tamil mani <t.karthikk10@gmail.com>
2025-05-20 11:51:06 -0700

    adding export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/studio',
+  assetPrefix: '/studio/',
   typescript: {
     ignoreBuildErrors: true,
   },
